1e7740c9db7bde0e42248d21c4db330ac6645417
paloma-vel <paloma.velazquez@gmail.com>
2018-11-12 18:27:20 -0800

    work in progress

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -5,7 +5,7 @@ describe('blinkyDancer', function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancer = new makeBlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it('should have a jQuery $node object', function() {
@@ -19,6 +19,7 @@ describe('blinkyDancer', function() {
   });
 
   describe('dance', function() {
+debugger;
     it('should call step at least once per second', function() {
       sinon.spy(blinkyDancer, 'step');
       expect(blinkyDancer.step.callCount).to.be.equal(0);
